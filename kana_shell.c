6c57f00ba1502d5cbb80284a81419a87eed8d839
Yuma Ohgami <yuma@ohgami.jp>
2018-07-05 00:03:20 +0900

    kana: add kbc_handler().

--- kana_shell.c
+++ kana_shell.c
@@ -1,8 +1,17 @@
 #include <fbcon.h>
 #include <font.h>
+#include <kbc.h>
+
+static void kbc_handler(char c)
+{
+	puth(c, 2);
+	putc(' ');
+}
 
 void kana_main(void)
 {
+	kbc_set_handler(kbc_handler);
+
 	unsigned char s[] = {
 		FONT_hira_ko,
 		FONT_hira_n,
