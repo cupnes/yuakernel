6b3763c75998dabdde3cdd7f8a7466955efa91e0
Yuma Ohgami <yuma@ohgami.jp>
2018-07-06 01:25:29 +0900

    fork kana_echoback to kana_shell.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include <hpet.h>
 #include <sched.h>
 #include <common.h>
-#include <kana_echoback.h>
+#include <kana_shell.h>
 
 struct __attribute__((packed)) platform_info {
 	struct framebuffer fb;
