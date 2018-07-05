dceff5a4181b0ad1e054d74d1d180a73396cae06
Yuma Ohgami <yuma@ohgami.jp>
2018-07-06 00:52:30 +0900

    fbcon: fix putc()'s argument.

--- include/fbcon.h
+++ include/fbcon.h
@@ -5,7 +5,7 @@
 #define VERTICAL_WRITING	1
 
 void set_text_direction(unsigned char test_direction);
-void putc(char c);
+void putc(char _c);
 void puts(char *s);
 void putd(unsigned long long val, unsigned char num_digits);
 void puth(unsigned long long val, unsigned char num_digits);
