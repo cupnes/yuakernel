3783953d0761d2a39c2b5045c0e6b3648feef979
Yuma Ohgami <yuma@ohgami.jp>
2018-10-14 18:46:23 +0900

    fbcon: add vertical writing functions.

--- fbcon.c
+++ fbcon.c
@@ -9,6 +9,19 @@
 unsigned int cursor_x = 0, cursor_y = 0;
 unsigned char font_size = FONT_SIZE_DEFAULT;
 
+void putc_pos(unsigned int pos_x, unsigned int pos_y, unsigned char c)
+{
+	unsigned int x, y;
+
+	for (y = 0; y < FONT_HEIGHT; y++) {
+		for (x = 0; x < FONT_WIDTH; x++) {
+			if (font_bitmap[(unsigned int)c][y][x])
+				draw_px_fg(pos_x + x, pos_y + y);
+
+		}
+	}
+}
+
 static void draw_char_default(unsigned int c)
 {
 	unsigned int x, y;
@@ -157,6 +170,74 @@ void puth(unsigned long long val, unsigned char num_digits)
 	str[num_digits] = '\0';
 
 	puts(str);
+}
+
+void vcursor_reset(void)
+{
+	cursor_x = fb.hr - FONT_WIDTH;
+	cursor_y = 0;
+}
+
+void vcr(void)
+{
+	cursor_y = 0;
+}
+
+unsigned char vlf(void)
+{
+	unsigned char do_clear_screen = 0;
+
+	if (cursor_x >= FONT_WIDTH) {
+		cursor_x -= FONT_WIDTH;
+	} else {
+		vcursor_reset();
+		do_clear_screen = 1;
+	}
+
+	return do_clear_screen;
+}
+
+unsigned char vupdate_cursor(void)
+{
+	unsigned char do_clear_screen = 0;
+
+	cursor_y += FONT_HEIGHT;
+	if ((cursor_y + FONT_HEIGHT) >= fb.vr) {
+		vcr();
+		do_clear_screen = vlf();
+	}
+
+	return do_clear_screen;
+}
+
+void vputc(unsigned char c)
+{
+	static unsigned char delayed_clear_screen = 0;
+	if (delayed_clear_screen) {
+		vcursor_reset();
+		clear_screen();
+		delayed_clear_screen = 0;
+	}
+
+	if (c == '\r') {
+		vcr();
+	} else if (c == '\n') {
+		unsigned char do_clear_screen = vlf();
+		if (do_clear_screen)
+			clear_screen();
+	} else {
+		/* カーソル座標(cursor_x,cursor_y)へ文字を描画 */
+		putc_pos(cursor_x, cursor_y, c);
+
+		/* カーソル座標の更新 */
+		delayed_clear_screen = vupdate_cursor();
+	}
+}
+
+void vputs(unsigned char *s)
+{
+	while (*s != '\0')
+		vputc(*s++);
 }
 
 void move_cursor(unsigned int x, unsigned int y)
--- include/fbcon.h
+++ include/fbcon.h
@@ -5,6 +5,9 @@ void putc(char c);
 void puts(char *s);
 void putd(unsigned long long val, unsigned char num_digits);
 void puth(unsigned long long val, unsigned char num_digits);
+void vcursor_reset(void);
+void vputc(unsigned char c);
+void vputs(unsigned char *s);
 void move_cursor(unsigned int x, unsigned int y);
 void set_font_size(unsigned char _font_size);
 
