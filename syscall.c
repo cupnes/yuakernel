c4b87c174b1dbc191070a05ef9bbc0d38902f897
Yuma Ohgami <yuma@ohgami.jp>
2018-10-30 21:57:31 +0900

    syscall: support putc.

--- syscall.c
+++ syscall.c
@@ -5,25 +5,29 @@
 
 #define SYSCALL_INTR_NO		128
 
+enum SYSCCALL_NO {
+	SYSCALL_PUT_CHAR,
+	MAX_SYSCALL_NUM
+};
+
 void syscall_handler(void);
 
 unsigned long long do_syscall_interrupt(
 	unsigned long long syscall_id, unsigned long long arg1,
 	unsigned long long arg2, unsigned long long arg3)
 {
-	puth(syscall_id, 16);
-	puts("\r\n");
-	puth(arg1, 16);
-	puts("\r\n");
-	puth(arg2, 16);
-	puts("\r\n");
-	puth(arg3, 16);
-	puts("\r\n");
+	unsigned long long ret_val = 0;
+
+	switch (syscall_id) {
+	case SYSCALL_PUT_CHAR:
+		putc((char)arg1);
+		break;
+	}
 
 	/* PICへ割り込み処理終了を通知(EOI) */
 	set_pic_eoi(SYSCALL_INTR_NO);
 
-	return 0xbeefcafefee1babe;
+	return ret_val;
 }
 
 void syscall_init(void)
