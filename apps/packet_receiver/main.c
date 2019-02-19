6a88bffce683d9385fe83beba0fb932a64806809
Yuma Ohgami <yuma@ohgami.jp>
2019-02-20 01:50:29 +0900

    packet_recver: remove kbc_handler.

--- apps/packet_receiver/main.c
+++ apps/packet_receiver/main.c
@@ -2,33 +2,23 @@
 
 #define PACKET_BUF_SIZE	1024
 
-static void kbc_handler(unsigned char c);
 static void dump_packet(unsigned char *p_buf, unsigned short p_len);
-
-unsigned char is_exit = 0;
 
 int main(void)
 {
-	set_kbc_handler(kbc_handler);
-
 	set_fg(255, 255, 255);
 	set_bg(0, 70, 250);
 	clear_screen();
 
 	unsigned char p_buf[PACKET_BUF_SIZE];
 	unsigned short p_len;
-	while (!is_exit) {
+	while (1) {
 		receive_packet(p_buf, &p_len);
 		if (p_len)
 			dump_packet(p_buf, p_len);
 	}
 
 	return 0;
-}
-
-static void kbc_handler(unsigned char c __attribute__((unused)))
-{
-	is_exit = 1;
 }
 
 static void dump_packet(unsigned char *p_buf, unsigned short p_len)
