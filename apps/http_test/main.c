9dc7ac3888d08db55ef02da5ba1547894e8b17ad
Yuma Ohgami <yuma@ohgami.jp>
2019-04-06 20:43:26 +0900

    apps: http_test: add http get.

--- apps/http_test/main.c
+++ apps/http_test/main.c
@@ -18,6 +18,7 @@
 
 #define TCP_HEADER_LEN_SHIFT	12
 #define TCP_FLAGS_SYN	0x0002
+#define TCP_FLAGS_PSH	0x0008
 #define TCP_FLAGS_ACK	0x0010
 
 struct __attribute__((packed)) ethernet_header {
@@ -102,6 +103,7 @@ unsigned char default_gw_mac[6] = {
 	DEFAULT_GW_MAC_3, DEFAULT_GW_MAC_4, DEFAULT_GW_MAC_5};
 unsigned char cupnes_com_ip[4] = {49, 212, 139, 172};
 /* 0x31, 0xd4, 0x8b, 0xac */
+char http_request[] = "GET /\r\n";
 
 unsigned char own_mac[6];
 unsigned char send_buf[PACKET_BUF_SIZE];
@@ -114,7 +116,7 @@ unsigned int swap_byte_4(unsigned int data);
 struct tcp_session *connect(
 	unsigned char dst_mac[], unsigned char dst_ip[],
 	unsigned short src_port, unsigned short dst_port);
-void http_test(struct tcp_session *session);
+void http_get(struct tcp_session *session);
 void disconnect(struct tcp_session *session);
 
 int main(void)
@@ -129,7 +131,7 @@ int main(void)
 
 	session = connect(default_gw_mac, cupnes_com_ip, SRC_PORT, HTTP_PORT);
 
-	/* http_test(session); */
+	http_get(session);
 
 	/* disconnect(session); */
 
@@ -302,9 +304,6 @@ void connect_ack(struct tcp_session *session)
 	struct tcp_header *tcp_h;
 	struct tcp_header_options_b *tcp_opt_h;
 
-	unsigned int i;
-	unsigned char *p;
-
 	/* frame */
 	base_addr = (unsigned long long)send_buf;
 
@@ -357,9 +356,57 @@ struct tcp_session *connect(
 	return session;
 }
 
-void http_test(struct tcp_session *session __attribute__((unused)))
+void http_get(struct tcp_session *session)
 {
-	return;
+	unsigned long long base_addr;
+	struct ip_header *ip_h;
+	struct tcp_header *tcp_h;
+	struct tcp_header_options_b *tcp_opt_h;
+	unsigned char *http_req_p;
+
+	/* frame */
+	base_addr = (unsigned long long)send_buf;
+
+	/* ip */
+	base_addr += sizeof(struct ethernet_header);
+	ip_h = (struct ip_header *)base_addr;
+	ip_h->total_length = swap_byte_2(52);
+	ip_h->identification = swap_byte_2(session->id);
+
+	/* tcp */
+	base_addr += sizeof(struct ip_header);
+	tcp_h = (struct tcp_header *)base_addr;
+	tcp_h->seq_num = swap_byte_4(session->seq_num);
+	tcp_h->ack_num = swap_byte_4(session->ack_num);
+	tcp_h->header_length_flags =
+		swap_byte_2((8U << TCP_HEADER_LEN_SHIFT) | TCP_FLAGS_PSH
+			    | TCP_FLAGS_ACK);
+	tcp_h->window_size = swap_byte_2(229);
+
+	/* tcp (options) */
+	base_addr += sizeof(struct tcp_header);
+	tcp_opt_h = (struct tcp_header_options_b *)base_addr;
+	tcp_opt_h->nop_1 = 1;
+	tcp_opt_h->nop_2 = 1;
+	tcp_opt_h->timestamp.kind = 8;
+	tcp_opt_h->timestamp.length = 10;
+	tcp_opt_h->timestamp.timestamp_value = swap_byte_4(145770);
+	tcp_opt_h->timestamp.timestamp_echo_replay = swap_byte_4(2512521363);
+
+	/* http */
+	base_addr += sizeof(struct tcp_header_options_b);
+	http_req_p = (unsigned char *)base_addr;
+	memcpy(http_req_p, http_request, sizeof(http_request));
+
+	unsigned short len =
+		sizeof(struct ethernet_header) + sizeof(struct ip_header)
+		+ sizeof(struct tcp_header)
+		+ sizeof(struct tcp_header_options_b) + sizeof(http_request);
+
+	send_packet(send_buf, len);
+
+	session->id++;
+	session->seq_num++;
 }
 
 void disconnect(struct tcp_session *session __attribute__((unused)))
